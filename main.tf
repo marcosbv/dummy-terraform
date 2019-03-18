c0ee11e48921bd724daedd24f7df92c50edd3df2
Marcos Vieira <marcosbv@br.ibm.com>
2019-03-17 21:53:53 -0300

    Initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+variable "number_of_instances" {
+  type = "string"
+}
+
+variable "min_of_instances" {
+  type = "string"
+}
+
+variable "max_of_instances" {
+  type = "string"
+}
+
+resource "null_resource" "dummy" {
+  triggers {
+    max_of_instances    = "${var.max_of_instances}"
+    min_of_instances    = "${var.min_of_instances}"
+    number_of_instances = "${var.number_of_instances}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo Instances=${var.number_of_instances}"
+  }
+}
